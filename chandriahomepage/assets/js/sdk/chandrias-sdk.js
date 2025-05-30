8b768d8e216df30b91a0a62a378a3b0abac3b665
lncadrnn <lncadrnn@users.noreply.github.com>
2025-05-30 11:28:47 +0800

    modernized index.html

--- chandriahomepage/assets/js/sdk/chandrias-sdk.js
+++ chandriahomepage/assets/js/sdk/chandrias-sdk.js
@@ -22,6 +22,8 @@ import {
     getDoc,
     query,
     where,
+    orderBy,
+    limit,
     arrayUnion,
     arrayRemove
 } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
@@ -58,6 +60,8 @@ export {
     collection,
     query,
     where,
+    orderBy,
+    limit,
     arrayUnion,
     arrayRemove,
     updateProfile
